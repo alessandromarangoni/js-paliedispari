de477a8d796a0c51f0454ef81fb08f9e0f8ddf4b
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-26 13:21:18 +0200

    aggiunto ciclo pallindrome commentato il resto

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ const parola = prompt("inserisci")
 // let parolaReverse = parolaDivisa.reverse('');
 // let parolaRiunita = parolaReverse.join('');
 
-var parolaInversa = invertiParola(parola);
-if(parola == parolaInversa){
+var parolaReverse = invertiParola(parola);
+if(parola == parolaReverse){
     alert("è palindroma");
 }
     else {
         alert("Non è palindroma");
     }
 
-function invertiParola(parola){
-    let parolaReverse = parola.split('').reverse(); 
-    let parolaRiunita = parolaReverse.join('');
-    return parolaRiunita;
+function invertiParola(parolaDainvertire){
+    // let parolaReverse = parola.split('').reverse(); 
+    // let parolaRiunita = parolaReverse.join('');
+    let parolaReverse='';
+
+    for (let i = parolaDainvertire.length-1; i >= 0; i--) {
+        const carattere = parolaDainvertire[i]
+        parolaReverse += carattere;
+    }
+
+    return parolaReverse;
 }
 
 /////////////pari dispari///////////
@@ -31,32 +38,47 @@ function funzione1(){
     return num1 + inserisciNumero;
 }
 
+let esito = funzione2(numSomm);
 
-if (inserisciNumero > 5 | inserisciNumero < 1)
-    { 
-        alert("inserisci un numero da 1 a 5");
+function funzione2(numero){
+    if (numSomm % 2 == 0 ){
+        return "pari"
     }
+    return "dispari"
+}
 
-    else{
-    ispariOrNot = ""
-    if (numSomm % 2 == 0){
-        var ispari = "pari"
-        if(pariODispari == ispari){
-            console.log("hai vinto")
-            alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI VINTO `)
-        }
-        else{console.log("hai perso")
-            alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI PERSO`)
-        }
-    }
+if(esito == pariODispari){
+    alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI VINTO `)
+}
+else{
+    alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI PERSO `)
+}
 
-    if (numSomm % 2 == 1){
-        var ispariOrNot = "dispari"
-        if(pariODispari == ispari){
-            console.log("hai vinto")
-            alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI VINTO `)
-        }
-        else{console.log("haiperso")
-            alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI PERSO`)
-        }
-    }}
+// if (inserisciNumero > 5 | inserisciNumero < 1)
+//     { 
+//         alert("inserisci un numero da 1 a 5");
+//     }
+
+//     else{
+//     ispariOrNot = ""
+//     if (numSomm % 2 == 0){
+//         var ispari = "pari"
+//         if(pariODispari == ispari){
+//             console.log("hai vinto")
+//             alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI VINTO `)
+//         }
+//         else{console.log("hai perso")
+//             alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI PERSO`)
+//         }
+//     }
+
+//     if (numSomm % 2 == 1){
+//         var ispariOrNot = "dispari"
+//         if(pariODispari == ispari){
+//             console.log("hai vinto")
+//             alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI VINTO `)
+//         }
+//         else{console.log("haiperso")
+//             alert(`hai scelto ${pariODispari} il numero del banco è ${num1} il tuo è ${inserisciNumero} HAI PERSO`)
+//         }
+//     }}
